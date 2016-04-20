d879a20e08b3226eb9ce08db621fc4fcb7968187
nomenclate <nomenclate@users.noreply.github.com>
2016-04-20 19:33:39 -0400

    minor changes to improve readability

--- test_aclparse.py
+++ test_aclparse.py
@@ -4,17 +4,21 @@ from aclparse import acl
 
 
 def test_name():
+    #test setup
     with open('aristaacl.txt', 'r') as f:
         showacltest = f.read()
 
-    parsedacl = acls.AccessList()
     result = acl.parseString(showacltest)
+
+    parsedacl = acls.AccessList()
     parsedacl.name = result['name']
 
     for n in result['entry']:
         parsedacl.entries.append(n.asDict())
 
     name = 'THIS_IS_A_NAME'
+
+    # Test results
     assert parsedacl.name == name
     assert result['name'] == name
     assert parsedacl.name == result['name']
